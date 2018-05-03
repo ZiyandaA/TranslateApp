02bb0f33c7a6762496580cdaadb9e6efc7f63542
Ziyanda Ayd <ziyandaayd@gmail.com>
2018-05-03 00:25:53 -0400

    Completed all changes to the App, made final styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,48 @@
 var GoogleTranslateApiUrl = 'https://translation.googleapis.com/language/translate/v2';
 var GoogleTextToSpeechApiUrl = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?fields=audioContent'
 var googleKey = 'AIzaSyDgiqOa-565SRqsU6x4EFE9E2ZUzDCyrls';
+var audio;
 $(document).ready(function(){
     $('.translateButton').click(function(e){
         e.preventDefault();
             var inputValue = $('.inputText').val();
             var targetLanguage = $('.inputLanguageCode option:selected').val();
-            console.log(targetLanguage);
+            var languageCode = $('.inputLanguageCode option:selected').data('code');
+
             if(inputValue!=""){
                 var requestUrl =GoogleTranslateApiUrl + "?key=" + googleKey +"&q="+inputValue+"&target="+targetLanguage;
                 $.ajax({
                     url: requestUrl
-                }).done(function(response){
+                }).done(function(response){console.log(response)
                     if(response.data != undefined && response.data['translations']!=undefined){
                         if(response.data['translations'][0]['translatedText']!=undefined){
                             var trasnlation = response.data['translations'][0]['translatedText'];
                             $('.outputText').text(trasnlation);
-                            textToSpeech(trasnlation);
+                            textToSpeech(trasnlation,languageCode);
                         }
                     }
                     
-                });
+           });
             }
     });
+    //
+    //$('.speaker').click(function(){
+    // audio.play();
+    //});
+
 });
 
-function textToSpeech(inputText){
+function textToSpeech(inputText,languageCode){
     var requestUrl = GoogleTextToSpeechApiUrl + "&key="+ googleKey;
     $.ajax({
         url: requestUrl,
         method: 'POST',
         data: JSON.stringify({
             'input':{
-              'text':'Android is a mobile operating system developed by Google, based on the Linux kernel and designed primarily for touchscreen mobile devices such as smartphones and tablets.'
+              'text':inputText
             },
             'voice':{
-              'languageCode':'en-gb',
-              'name':'en-GB-Standard-A',
+              'languageCode':languageCode,
               'ssmlGender':'FEMALE'
             },
             'audioConfig':{
@@ -45,7 +51,13 @@ function textToSpeech(inputText){
           }),
           contentType: "application/json; charset=utf-8",
           dataType   : "json",
-    }).done(function(response){
-        console.log(response);
+          error: function(res){
+              console.log(res.responseText)
+              $(".AudioButton").removeClass("btn-success").addClass("btn-disabled").text("No Audio").off("click");
+          }
+    }).done(function(response){ console.log(response)
+        audio = new Audio("data:audio/wav;base64," + response.audioContent);
+        audio.play();
+        $(".AudioButton").removeClass("btn-disabled").addClass("btn-success").text("Play Audio").on("click", function(){audio.play()});
     });
 }
